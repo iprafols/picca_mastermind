bf67ab6bf69b6f782db0b2b7325e69125919c3fd
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2024-07-12 15:54:19 +0200

    modified __init__.py to have correct imports

---
+++ picca_mastermind/__init__.py
@@ -0,0 +1,4 @@
+# This is so that we can use vega with `from picca_mastermind import vega`
+from .vega import vega
+# This is so that we can use qsonic with `from picca_mastermind import qsonic`
+from .qsonic.py import qsonic
